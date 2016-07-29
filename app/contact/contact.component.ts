59fbe83c33831586e9ed1021c346d79d82addc2e
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-07-29 11:03:28 +0700

    ..

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    templateUrl: '/app/contact.component.html'
+    templateUrl: '/app/contact/contact.component.html'
 })
 export class ContactComponent {
     onSubmit(form: any) {
