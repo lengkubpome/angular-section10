d771284b826b94696b3fd0daf67fd2a14f1185f3
Kiattiphong Saetang <lengkubpome@gmail.com>
2016-08-01 00:44:24 +0700

    ...

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,6 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { HeroService } from './heroes/hero.service';
 
-
-
-// import { AlbumComponent } from './albums/album.component';
-// import { AlbumsComponent } from './albums/albums.component';
-// import { ContactComponent } from './contact/contact.component';
-
-
 @Component({
     selector: 'my-app',
     templateUrl: `/app/app.component.html`,
